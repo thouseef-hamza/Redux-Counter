d5da01aba89074a5973bbf9912bb4886fb65cd5b
Thouseef <thousithouseef731@gmail.com>
2023-09-07 12:23:46 +0530

    Redux-Counter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Provider } from 'react-redux'
+import './App.css'
+import Counter from './app/counter/counter'
+function App() {
+
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
+
+export default App
